f26941aa7b4e7120c1a614715ec5f4a998a3b741
SDF-Git <tylerfrancis@unomaha.edu>
2026-09-16 23:15:21 -0500

    Commit Initial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<router-view/>
+</template>
+
+<script>
+import router from "./router";
+export default {
+  name: 'app',
+  props: {
+    msg: String
+  },
+mounted(){
+  router.push("/skill-list");
+  }
+}
+</script>
+<style>
+</style>
